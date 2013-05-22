ba2f2b4c3114b6ea93d6338702d2533c976637c1
Dan Tao <daniel.tao@gmail.com>
2013-05-21 19:29:18 -0700

    added lines method to StreamedSequence (see #13)
    has a bug right now (I think), but I'll fix it later

--- lazy.node.js
+++ lazy.node.js
@@ -1,13 +1,17 @@
 var fs = require("fs");
 
-var StreamedSequence = global.Lazy.Sequence.inherit(function(path, encoding) {
+var Lazy = global.Lazy;
+
+var StreamedSequence = Lazy.Sequence.inherit(function(path, encoding) {
   this.path = path;
   this.encoding = encoding;
 });
 
 StreamedSequence.prototype.each = function(fn) {
-  var stream = fs.createReadStream(this.path);
-  stream.setEncoding(this.encoding);
+  var stream = fs.createReadStream(this.path, {
+    encoding: this.encoding,
+    autoClose: true
+  });
 
   var listener = function(e) {
     if (fn(e) === false) {
@@ -18,6 +22,33 @@ StreamedSequence.prototype.each = function(fn) {
   stream.on("data", listener);
 };
 
-global.Lazy.stream = function(path, encoding) {
+var StreamedLineSequence = Lazy.Sequence.inherit(function(parent) {
+  this.parent = parent;
+});
+
+StreamedLineSequence.prototype.each = function(fn) {
+  this.parent.each(function(data) {
+    var finished = false;
+
+    // TODO: I'm pretty sure there's a bug here: if/when the buffer ends in the
+    // middle of a line, this will artificially split that line in two. I'll
+    // come back to this later.
+    Lazy(data).split("\n").each(function(line) {
+      if (fn(line) === false) {
+        finished = true;
+        return false;
+      }
+    });
+    if (finished) {
+      return false;
+    }
+  });
+};
+
+StreamedSequence.prototype.lines = function() {
+  return new StreamedLineSequence(this);
+};
+
+Lazy.stream = function(path, encoding) {
   return new StreamedSequence(path, encoding);
 };
