5114d954d3638bea871fed2bd22505b31439ffe0
Dan Tao <daniel.tao@gmail.com>
2013-10-25 16:46:13 -0700

    added handling of ObjectLikeSequence for equality checks

--- docs/doc_helper.js
+++ docs/doc_helper.js
@@ -1,7 +1,9 @@
 Benchmark.options.maxTime = 1;
 
 function assertEquality(actual, expected) {
-  if (actual instanceof Lazy.Sequence) {
+  if (actual instanceof Lazy.ObjectLikeSequence) {
+    actual = actual.toObject();
+  } else if (actual instanceof Lazy.Sequence) {
     actual = actual.toArray();
   }
   expect(actual).toEqual(expected);
