00db06251bc0f16749920f18c3c4888f858d3920
Dan Tao <daniel.tao@gmail.com>
2013-12-19 18:33:12 -0800

    forget this live reloading crap

--- site/config.rb
+++ site/config.rb
@@ -23,9 +23,6 @@ helpers do
   end
 end
 
-# Reload the browser automatically whenever files change
-activate :livereload
-
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
