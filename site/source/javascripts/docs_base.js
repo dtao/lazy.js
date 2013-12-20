45582e2b6cf9151d890dbff72bed5665ed8fb6f8
Dan Tao <daniel.tao@gmail.com>
2013-12-19 18:18:05 -0800

    working on getting docs to behave w/ latest autodoc

---
+++ site/source/javascripts/docs_base.js
@@ -0,0 +1,7 @@
+//= require lib/jasmine
+//= require lib/lodash
+//= require lib/benchmark
+//= require lib/jquery
+//= require lib/highcharts
+//= require lib/hightables
+//= require docs/doc_helper
