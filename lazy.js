9f33dbb2d27eed441a61c6d7a66e2ee7e1eaa957
Dan Tao <daniel.tao@gmail.com>
2013-12-30 16:54:43 -0800

    added example to #flatten for sequences within sequences

--- lazy.js
+++ lazy.js
@@ -1406,6 +1406,7 @@
    *
    * @examples
    * Lazy([1, [2, 3], [4, [5]]]).flatten() // sequence: [1, 2, 3, 4, 5]
+   * Lazy([1, Lazy([2, 3])]).flatten()     // sequence: [1, 2, 3]
    */
   Sequence.prototype.flatten = function flatten() {
     return new FlattenedSequence(this);
