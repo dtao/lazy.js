a4a1609d571fc3f53954c223ab69f46b4693fb51
Dan Tao <daniel.tao@gmail.com>
2014-01-03 12:55:35 -0800

    fixed a nasty bug (!) in #all

--- lazy.js
+++ lazy.js
@@ -1705,11 +1705,19 @@
    *
    * var numbers = [1, 2, 3, 4, 5];
    *
+   * var objects = [{ foo: true }, { foo: false, bar: true }];
+   *
    * Lazy(numbers).every(isEven)     // => false
    * Lazy(numbers).every(isPositive) // => true
+   * Lazy(objects).all('foo')        // => false
+   * Lazy(objects).all('bar')        // => false
    */
   Sequence.prototype.every = function every(predicate) {
-    return this.each(createCallback(predicate));
+    predicate = createCallback(predicate);
+
+    return this.each(function(e, i) {
+      return !!predicate(e, i);
+    });
   };
 
   Sequence.prototype.all = function all(predicate) {
