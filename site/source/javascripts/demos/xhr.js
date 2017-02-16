36bab6c3359d6d96ff3fb423fa17e5027d9df6e2
Daniel Tao <daniel.tao@gmail.com>
2017-02-16 10:25:19 -0600

    updated XHR demo to leverage promise result from Lazy.makeHttpResponse

--- site/source/javascripts/demos/xhr.js
+++ site/source/javascripts/demos/xhr.js
@@ -10,12 +10,23 @@
     return encodeURIComponent(k) + '=' + encodeURIComponent(v);
   }).join('&');
 
-  lazy.makeHttpRequest(url).each(function(chunk) {
-    var el = document.createElement('div');
-    el.className = 'chunk';
-    el.textContent = chunk;
+  var iterateChunks = lazy.makeHttpRequest(url)
+    .filter(function(chunk) {
+      return chunk.length > 0;
+    })
+    .map(function(chunk) {
+      return chunk.length + ' bytes: ' + chunk; 
+    })
+    .each(function(chunk) {
+      var el = document.createElement('div');
+      el.className = 'chunk';
+      el.textContent = chunk;
 
-    document.getElementById('chunks').appendChild(el);
+      document.getElementById('chunks').appendChild(el);
+    });
+
+  iterateChunks.then(function() {
+    document.getElementById('state').textContent = 'Done!';
   });
 
 }(Lazy));
